7e2e6bc2d26fabfbd6b2f33ea7d25ae6ca2789af
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-20 21:10:08 +0530

    first commit

---
+++ ArraysAssignment/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using ClassLibrary1;
+namespace ArraysAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+           Car car1 = new Car() { Make="Toyoto",Model="Corolla",RentalPrice=50,Rentted=true};
+            Car car2 = new Car() { Make = "Honda", Model = "Civic", RentalPrice = 80, Rentted = true };
+            Car car3 = new Car() { Make = "Ford", Model = "Mustang", RentalPrice = 80, Rentted = true };
+            Console.WriteLine("Welcome to the car rental System");
+            Console.WriteLine();
+            int Value = 0;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Rent a car");
+                Console.WriteLine("2. Return a car");
+                Console.WriteLine("3. View avalable cars");
+                Console.WriteLine("4. Exit");
+                Console.Write("Please Chose your one option : ");
+                Value = int.Parse(Console.ReadLine());
+                Console.WriteLine(Value);
+
+                {
+                    Car[] array = new Car[3] { car1, car2, car3 };
+                    switch (Value)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Rent");
+                               
+                                Console.WriteLine("Rent Car");
+                                Console.WriteLine("1. Toyoto Corolla (Rental price: 50/day) : "+car1.Rentted);
+                                Console.WriteLine("2. Honda Civic (Rental price: 80/day) : "+car2.Rentted);
+                                Console.WriteLine("3. Ford Mustang (Rental price: 80/day) : "+car3.Rentted);
+                                Console.Write("chose your Id : ");
+                                int id1=int.Parse(Console.ReadLine());
+                                array[id1-1].Rent();
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Return Car");
+                            Console.WriteLine("1. Toyoto Corolla (Rental price: 50/day) : " + car1.Rentted);
+                            Console.WriteLine("2. Honda Civic (Rental price: 80/day) : " + car2.Rentted);
+                            Console.WriteLine("3. Ford Mustang (Rental price: 80/day) : " + car3.Rentted);
+                            Console.Write("chose your Id : ");
+                            int id2 = int.Parse(Console.ReadLine());
+                            array[id2 - 1].Return();
+                            break;
+                        case 3:
+                            Console.WriteLine("View");
+                            Console.WriteLine("Available Car");
+                            Console.WriteLine("1. Toyoto Corolla (Rental price: 50/day) : " + car1.Rentted);
+                            Console.WriteLine("2. Honda Civic (Rental price: 80/day) : " + car2.Rentted);
+                            Console.WriteLine("3. Ford Mustang (Rental price: 80/day) : " + car3.Rentted);
+                            
+                            break;
+                        default:
+                            Console.WriteLine("Exit");
+                            break;
+
+                    }
+                }
+            } while (Value != 4);
+
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private decimal rentalprice;
+        private bool rented;
+
+        public string Make { set { make = value; } get { return make; } }
+        public string Model { set { model = value; } get { return model; } }
+        public decimal RentalPrice { set { rentalprice = value; } get { return rentalprice; } }
+        public bool Rentted { set { rented = value; } get { return rented; } }
+
+        public void Rent()
+        {
+
+            if (Rentted == true)
+            {
+                Console.WriteLine("selected car is available for rent");
+                this.Rentted = false;
+            }
+            else
+            {
+                Console.WriteLine("sorry, selected car is not available for rent");
+            }
+        }
+        public void Return()
+        {
+            if(Rentted == false)
+            {
+                Console.WriteLine("Thanks for returning the car");
+                this.Rentted = true;
+            }
+            else
+            {
+                Console.WriteLine("Car is available");
+            }
+        }
+       
+    }
+}
